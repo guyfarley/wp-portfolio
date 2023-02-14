789c8054c3a4cf27cebd166f592fe95f5ceb713a
Guy Farley <guy.e.farley@gmail.com>
2023-02-14 11:20:44 -0800

    front page

---
+++ app/public/wp-content/themes/portfolio-theme/header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/noscript.css" />
+	</noscript>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
